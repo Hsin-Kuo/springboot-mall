c67abb3f4c487a2e244cd72eb7f7174deb53c583
Hsin-Kuo <ketty31kuo@gmail.com>
2024-05-03 19:44:14 +0800

    帳號功能 - 檢查註冊的email、隱藏返回的密碼

--- src/main/java/com/hsinkuo/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/hsinkuo/springbootmall/service/impl/UserServiceImpl.java
@@ -4,18 +4,33 @@ import com.hsinkuo.springbootmall.dao.UserDao;
 import com.hsinkuo.springbootmall.dto.UserRegisterRequest;
 import com.hsinkuo.springbootmall.model.User;
 import com.hsinkuo.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        //檢查註冊的email
+        if (user != null){
+            log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        }
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
